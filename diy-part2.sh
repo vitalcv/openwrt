a4c03e4470a5f2250d83e7c46b8513f7e03889b3
vitalcv <126236231+vitalcv@users.noreply.github.com>
2024-08-25 19:30:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,3 +20,5 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+
+git clone https://github.com/linkease/istore.git package/istore
